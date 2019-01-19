e8e7d9ed6d8fadd37f1134b8317a3d86ee484675
Jesperjensensvard <43741171+Jesperjensensvard@users.noreply.github.com>
2019-01-19 13:01:09 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<style>
+    h1{
+        position: relative;
+        left: 18em;
+    }
+
+    h2{
+        position: relative;
+        left: 8em;
+        margin-top: 6em;
+    }
+    div{
+        display:flex;
+        text-align: center;
+        justify-content: center;
+        background-color: #212121;
+        color: white;
+        height: 100vh;
+    }
+    form {
+        margin-top: 15em;
+    }
+    
+    #apa {
+        position: relative;
+        right: 16em;
+    }
+
+    #tiger{
+        position: relative;
+        right: 28.6em;
+        top:2em;
+    }
+
+    #giraff{
+        position: relative;
+        right: 41.2em;
+        top:4em;
+    }
+
+    #nuts {
+        position: relative;
+        right: 28.4em;
+        top:4.4em;
+    }
+
+    #leButt {
+        position: relative;
+        right: 28.5em;
+        top:4.4em;
+        background-color: #3232ff;
+        border-radius: 5px;
+        border-color: #3232ff;
+        color:white;
+    }
+   
+
+</style>
+<div>
+<h1>Skapa ditt zoooo</h1>
+<h2>lägg till hur många djur du vill och hur mycket mat de vill ha</h2>
+
+<form action="results.php" method="POSt">
+    <input id="apa" type="number" name="apor">
+    <input id="tiger" type="number" name="tiger">
+    <input id="giraff"type="number" name="giraff">
+    <input id="nuts" type="number" name="nut">
+    <br><br>
+    <button type="submit" id="leButt">visa mitt zoo</button>
+</form>
+
+
+</div>
+  
+</body>
+</html>
---
+++ results.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<style>
+    img {
+        width: 5em;
+        height: 8em;
+    }
+</style>
+
+<?php
+$mainApor = $_POST['apor'];
+$mainTiger = $_POST['tiger'];
+$mainGiraff = $_POST['giraff'];
+$mainNUt = $_POST['nut'];
+
+abstract class Animal {
+    protected $type;
+    abstract function sound();
+}
+
+
+class apa extends Animal {
+    private $bild;
+
+    function __construct( $bild){
+        $this->type = $bild;
+    }
+
+  
+    public function get_type() {
+        return $this->type;
+    }
+
+    function sound() {
+        return " <p>my sound is:</p>" ." <p>hohohohoohoho</p>";
+    }
+
+}
+
+class tiger extends Animal {
+    private $bild; 
+
+    function __construct($bild) {
+        $this->type = $bild;
+    }
+
+    public function get_bild() {
+        return $this->type;
+    }
+
+    function sound() {
+        return " <p>my sound is:</p>" ." <p>rawwwr</p>";
+    }
+}
+
+class giraff extends Animal {
+    private $bild;
+
+    function __construct($bild) {
+        $this->type = $bild;
+    }
+
+    public function get_giraff() {
+        return $this->type;
+    }
+
+    function sound() {
+        return " <p>my sound is:</p>" ." <p>pfffffff</p>";
+
+    }
+}
+
+
+
+class nuts extends Animal {
+    private $bild;
+
+    function __construct($bild){
+        $this->type = $bild;
+    }
+
+    public function get_TheNuts() {
+        return $this->type; 
+    }
+
+    function sound() {
+        return "<p> my sound is: </p>" . "<p>puuffffff</p>" . "<p> But only if i fall </P>";
+    }
+}
+
+$newApa = new apa( '<img src="apa.jpg"> <br>');
+$newTiger = new tiger('<img src="tiger.jpg"> <br>');
+$newGiraff = new giraff('<img src="giraff.jpg"> <br>');
+$newNut = new nuts('<img src="nut.jpg"> <br>');
+
+$finalape =  $newApa->get_type() . "" . $newApa->sound();
+$finaltiger = $newTiger->get_bild() . "" . $newTiger->sound();
+$finalgiraff = $newGiraff->get_giraff() ."". $newGiraff->sound();
+$finalnut = $newNut->get_TheNuts() ."". $newNut->sound();
+
+for ($i=0; $i < $mainApor; $i++) { 
+    echo $finalape;
+}
+
+
+for ($i=0; $i < $mainTiger; $i++) { 
+    echo $finaltiger;
+}
+
+for ($i=0; $i < $mainGiraff; $i++) { 
+    echo $finalgiraff;
+}
+
+for ($i=0; $i < $mainNUt; $i++) { 
+    echo $finalnut;
+}
+?>
+</body>
+</html>
